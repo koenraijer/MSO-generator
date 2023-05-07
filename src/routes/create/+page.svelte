4286a0680328fe44f6454e01ccd7261f690a57da
Koen Raijer <koenrayer@gmail.com>
2023-05-07 18:54:07 +0200

    Added explanations and stuff

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -12,7 +12,7 @@
     import { goto } from "$app/navigation";
     import { focusTrap } from '@skeletonlabs/skeleton';
     import { popup } from '@skeletonlabs/skeleton';
-
+    import {onMount} from 'svelte';
     let shiftKeyPressed = false;
     let isFocused = true;
 
@@ -68,7 +68,6 @@
         };
 
         if (elementType === "select" && $selectOptions.length === 0) {
-                console.log('Please add at least one option for the select element.')
                 const t = {
                     message: 'Please add at least one option for the select element.',
                     background: 'variant-filled-warning',
@@ -145,7 +144,6 @@
             // TRUE if confirm pressed, FALSE if cancel pressed
             response: (r) => {
                 if (r) {
-                    console.log('Clearing memory...');
                     localStorage.clear();
                     paragraphs.set([])
                     addParagraph();
@@ -205,7 +203,7 @@
         }
     }
     
-    let popupSettings = {
+    let menuPopupSettings = {
         // Set the event as: click | hover | hover-click | focus | focus-click
         event: 'hover-click',
         // Provide a matching 'data-popup' value.
@@ -216,24 +214,114 @@
     let ulHeight;
     $: leftColumnStyle = `max-height: ${rightColumnHeight}px;`;
 
-    $: console.log($paragraphs.length)
-    $: console.log(ulHeight > rightColumnHeight, ulHeight, rightColumnHeight)
+    // Information popups
+    let informationContents = [
+        {
+            title: 'What is this?',
+            content: 'This is a simple tool to create standard texts/templates for those that do not have the luxury of an EHR with this functionality built-in. You can create a standard text by adding paragraphs and elements to the paragraphs.',
+        },
+        {
+            title: 'Add, rename and delete paragraphs.',
+            content: `Select the paragraphs you want to add elements to by pressing on the gray circle to the left of its name. \n\n Rename paragraphs by clicking on the paragraph's name. Delete them by clicking on the trashcan to its right. A paragraph provides structure to your standard text, but it will not be part of the final text.`,
+        },
+        {
+            title: 'What are elements?',
+            content: 'An element is a part of a paragraph. They come in three types: text, select elements and yes/no elements.',
+        },
+        {
+            title: 'What is a text element?',
+            content: `A text element is simply that: normal text. Use it when you want to add text to a paragraph that doesn't need any special formatting.`,
+        },
+        {
+            title: 'What is a select element?',
+            content: 'A select element allows you to choose between multiple options. They are the reason standard texts are so useful.',
+        },
+        {
+            title: 'What is a yes/no element?',
+            content: 'A yes/no element allows you to define a piece of text that is only added to the final text if you want it to.',
+        },
+        {
+            title: 'Preview your standard text.',
+            content: 'This shows what your standard text would look like if you would generate it right now.',
+        }
+    ]
+    let info1PopupSettings = {
+        // Set the event as: click | hover | hover-click | focus | focus-click
+        event: 'click',
+        // Provide a matching 'data-popup' value.
+        target: 'info1Popup'
+    };
+    let info2PopupSettings = {
+        // Set the event as: click | hover | hover-click | focus | focus-click
+        event: 'click',
+        // Provide a matching 'data-popup' value.
+        target: 'info2Popup'
+    };
+    let info3PopupSettings = {
+        // Set the event as: click | hover | hover-click | focus | focus-click
+        event: 'click',
+        // Provide a matching 'data-popup' value.
+        target: 'info3Popup'
+    };
+    let info4PopupSettings = {
+        // Set the event as: click | hover | hover-click | focus | focus-click
+        event: 'click',
+        // Provide a matching 'data-popup' value.
+        target: 'info4Popup'
+    };
+
+    const showWelcomeToast = () => {
+        const t = {
+        message: 'Welcome! This is a simple tool to create standard texts for healthcare professionals dealing with archaic EHR software that does not have this functionality built-in. luxury of an EHR with this functionality built-in. Click on the information icons to learn more about the different features.',
+        autohide: false
+        };
+        toastStore.trigger(t);
+    };
+
+    onMount(() => {
+        if (localStorage.getItem('firstVisit') === null) {
+        showWelcomeToast();
+        localStorage.setItem('firstVisit', 'false');
+        }
+    });
+
   </script>
   
 <div class="container mx-auto p-8 space-y-8 max-w-3xl flex-col flex-nowrap">
 
-        <button use:popup={popupSettings} class="absolute top-0 right-6 my-6 bg-surface-200 p-2 rounded-full aspect-square text-xl">
+        <button use:popup={menuPopupSettings} class="absolute top-0 right-6 my-6 bg-surface-200 p-2 rounded-full aspect-square text-xl">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="w-5 h-5" viewBox="0 0 256 256"><path d="M144,128a16,16,0,1,1-16-16A16,16,0,0,1,144,128ZM60,112a16,16,0,1,0,16,16A16,16,0,0,0,60,112Zm136,0a16,16,0,1,0,16,16A16,16,0,0,0,196,112Z"></path></svg>
         </button>
-        <div data-popup="menuPopup" class="-translate-x-4 -translate-y-4">
-            <button class="btn variant-filled-surface" on:click={clearMemory}>Clear memory</button>
+        <div data-popup="menuPopup" class="-translate-x-4 -translate-y-6 w-56 card flex-col !justify-items-end !flex gap-y-4 p-2">
+            <button class="btn variant-filled-surface rounded-md" on:click={clearMemory}>Clear memory</button>
+            <a href="/mso" class="btn variant-filled-surface rounded-md" on:click={clearMemory}>Mentale status onderzoek</a>
         </div>
 
     <div>
-        <h1 class="text-2xl mb-8 md:mb-12">The standard text generator</h1>
+        <h1 class="text-2xl mb-8 md:mb-12">
+            The standard text generator
+            <svg use:popup={info3PopupSettings} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="w-5 h-5 inline fill-surface-400 -translate-y-[0.125rem]" viewBox="0 0 256 256"><path d="M108,84a16,16,0,1,1,16,16A16,16,0,0,1,108,84Zm128,44A108,108,0,1,1,128,20,108.12,108.12,0,0,1,236,128Zm-24,0a84,84,0,1,0-84,84A84.09,84.09,0,0,0,212,128Zm-72,36.68V132a20,20,0,0,0-20-20,12,12,0,0,0-4,23.32V168a20,20,0,0,0,20,20,12,12,0,0,0,4-23.32Z"></path></svg>
+        </h1>
+        <div data-popup="info3Popup" class="card variant-filled-secondary w-72">
+            <header class="card-header">
+                <h4>{informationContents[0].title}</h4>
+            </header>
+            <section class="p-4">{informationContents[0].content}</section>
+            <div class="arrow variant-filled-secondary" />
+        </div>
         <div class="grid grid-cols-1 md:grid-cols-5 gap-x-4 md:gap-x-8 lg:gap-x-12"> 
-            <div class="flex flex-col col-span-2 md:w-fit w-full h-auto pb-8 md:pb-0" style={leftColumnStyle} bind:clientHeight="{ulHeight}">
-                <span class="block mb-1">Paragraphs</span>
+            <div class="flex flex-col col-span-2 md:w-fit w-full h-auto pb-8 md:pb-0 !z-100" style={leftColumnStyle} bind:clientHeight="{ulHeight}">
+                <span class="block mb-1">
+                    Paragraphs
+                    <svg use:popup={info1PopupSettings} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="w-5 h-5 inline fill-surface-400 -translate-y-[0.125rem]" viewBox="0 0 256 256"><path d="M108,84a16,16,0,1,1,16,16A16,16,0,0,1,108,84Zm128,44A108,108,0,1,1,128,20,108.12,108.12,0,0,1,236,128Zm-24,0a84,84,0,1,0-84,84A84.09,84.09,0,0,0,212,128Zm-72,36.68V132a20,20,0,0,0-20-20,12,12,0,0,0-4,23.32V168a20,20,0,0,0,20,20,12,12,0,0,0,4-23.32Z"></path></svg>
+                </span>
+                <div data-popup="info1Popup" class="card variant-filled-secondary w-72">
+                    <header class="card-header">
+                        <h4>{informationContents[1].title}</h4>
+                    </header>
+                    <section class="p-4">{informationContents[1].content}</section>
+                    <div class="arrow variant-filled-secondary" />
+                </div>
                 <ul
                 class="h-fit btn-group-vertical bg-surface-100 overflow-x-none rounded-md overflow {ulHeight >= rightColumnHeight ? 'overflow-y-scroll' : 'overflow-y-none'}"
                 >
@@ -243,13 +331,13 @@
                 >
                     <div class="flex flex-row flex-nowrap">
                     <button on:click={() => switchParagraph(index)}>
-                        <span class="border rounded-full border-surface-800 p-2 {currentParagraphIndex === index ? '!bg-surface-800' : ''}"></span>
+                        <span class="border rounded-full border-surface-400 p-2 {currentParagraphIndex === index ? '!bg-surface-400' : ''}"></span>
                     </button>
                     <input
                         type="text"
                         bind:value={paragraph.name}
                         class="input !border-none rounded-md w-full bg-transparent"
-                        placeholder={`Paragraph ${index + 1}`}
+                        placeholder="Enter paragraph title..."
                         on:input={() => updateParagraphName(index, paragraph.name)}
                     />
                     <span
@@ -294,10 +382,36 @@
                 </button>
             </div>
             <div class="col-span-3 h-fit" bind:clientHeight={rightColumnHeight}>
-                <hr class="md:invisible visible my-8">
-                <form use:focusTrap={isFocused}>
+                <div class="md:invisible visible">
+                    <hr class="md:my-0 my-8">
+                </div>
+
+                <div data-popup="info2Popup" class="card variant-filled-secondary w-96">
+                    <header class="card-header">
+                        <h4>{informationContents[2].title}</h4>
+                    </header>
+                    <section class="p-4">{informationContents[2].content}</section>
+                    <section class="p-4">
+                        <h5 class="font-semibold mb-1">{informationContents[3].title}</h5>
+                        <p>{informationContents[3].content}</p>
+                    </section>
+                    <section class="p-4">
+                        <h5 class="font-semibold mb-1">{informationContents[4].title}</h5>
+                        <p>{informationContents[4].content}</p>
+                    </section>
+                    <section class="p-4">
+                        <h5 class="font-semibold mb-1">{informationContents[5].title}</h5>
+                        <p>{informationContents[5].content}</p>
+                    </section>
+                    <div class="arrow variant-filled-secondary" />
+                </div>
+                <form use:focusTrap={isFocused} class="h-full">
                     <div class="mb-6 md:mb-8 w-fit">
-                        <label for="type" class="block mb-1">Element Type</label>
+                        <div class="inline -translate-y-2">
+                            <label for="type" class="inline">Element Type
+                            </label>
+                            <svg use:popup={info2PopupSettings} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="w-5 h-5 inline fill-surface-400 -translate-y-[0.125rem]" viewBox="0 0 256 256"><path d="M108,84a16,16,0,1,1,16,16A16,16,0,0,1,108,84Zm128,44A108,108,0,1,1,128,20,108.12,108.12,0,0,1,236,128Zm-24,0a84,84,0,1,0-84,84A84.09,84.09,0,0,0,212,128Zm-72,36.68V132a20,20,0,0,0-20-20,12,12,0,0,0-4,23.32V168a20,20,0,0,0,20,20,12,12,0,0,0,4-23.32Z"></path></svg>
+                        </div>
                         <select
                             bind:value={elementType}
                             id="type"
@@ -383,13 +497,31 @@
             </div>
         </div>
     
-        <hr class="my-8">
+        <hr class="my-4 md:my-6">
 
-        <div class="">
-            <h2></h2>
+        <label class="mb-2">
+            Preview
+            <svg use:popup={info4PopupSettings} xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="w-5 h-5 inline fill-surface-400 -translate-y-[0.125rem]" viewBox="0 0 256 256"><path d="M108,84a16,16,0,1,1,16,16A16,16,0,0,1,108,84Zm128,44A108,108,0,1,1,128,20,108.12,108.12,0,0,1,236,128Zm-24,0a84,84,0,1,0-84,84A84.09,84.09,0,0,0,212,128Zm-72,36.68V132a20,20,0,0,0-20-20,12,12,0,0,0-4,23.32V168a20,20,0,0,0,20,20,12,12,0,0,0,4-23.32Z"></path></svg>
+        </label>
+        <div data-popup="info4Popup" class="card variant-filled-secondary w-72">
+            <header class="card-header">
+                <h4>{informationContents[6].title}</h4>
+            </header>
+            <section class="p-4">{informationContents[6].content}</section>
+            <div class="arrow variant-filled-secondary" />
+        </div>
+        <div class="bg-surface-100 p-4 rounded-md">
             {#each $paragraphs as paragraph, paragraphIndex}
-            <h3 class="my-4">{paragraph.name}</h3>
+            {#if paragraph.name}
+                <h3 class:mb-4={paragraphIndex === 0} class:mt-0={paragraphIndex === 0} class:my-4={paragraphIndex !== 0}>{paragraph.name}</h3>
+            {:else}
+                <h3 class="my-4 text-surface-600" class:mb-4={paragraphIndex === 0} class:mt-0={paragraphIndex === 0} class:my-4={paragraphIndex !== 0}>Nameless paragraph</h3>
+            {/if}
             <p>
+            <!--If no paragraph contains elements-->
+            {#if paragraph.elements.length === 0}
+                <span class="text-surface-400">Start by adding some elements to this paragraph...</span>
+            {/if}
             {#each paragraph.elements as element, elementIndex}
                 <span class="group inline relative">
                     {#if element.type === 'text'}
@@ -418,7 +550,7 @@
 
     </div>
 
-    <button class="btn rounded-md w-full flex items-center text-white variant-filled-tertiary" on:click={createStandardText}>
+    <button class="btn rounded-md w-full flex items-center text-white variant-filled-primary" on:click={createStandardText}>
         <div class="flex flex-row flex-nowrap w-fit">
             {#if loading}
             <Moon class="absolute inset-0 mx-auto my-auto" size=20 borderWidth=2 />
